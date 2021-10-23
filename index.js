5aef9bc9f4cefe0077171f4ccdc39b0e20655765
Pradhumansinh Padhiyar <pradhu619@gmail.com>
2021-10-23 20:28:06 +0530

    Basic Bot Integaration

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const { Telegraf } = require('telegraf');
 const app = express();
+require('dotenv').config({path: __dirname + '/.env'})
 const product = require('./api/product');
+
 
 app.use(express.json({extend:false}));
 
-app.use("/api/product", product);
+// app.use("/api/product", product);
 const PORT = process.env.PORT || 8080;
+
+
+const bot = new Telegraf(process.env['BOT_TOKEN'])
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
+
 app.listen(PORT, ()=> console.log('Server Running at '+PORT));
