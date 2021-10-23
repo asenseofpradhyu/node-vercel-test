dc0c9455cdfc6b1bb33a01409afe9d8e0a744e7f
Pradhumansinh Padhiyar <pradhu619@gmail.com>
2021-10-23 20:52:40 +0530

    Basic Bot Integaration 3

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const TelegramBot = require('node-telegram-bot-api');
 
 app.use(express.json({extend:false}));
 
-// app.use("/api/product", product);
+app.use("/api/product", product);
 const PORT = process.env.PORT || 8080;
 
 // Telegraf Lib Demo
@@ -18,33 +18,7 @@ const PORT = process.env.PORT || 8080;
 // bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 // bot.launch()
 
-// replace the value below with the Telegram token you receive from @BotFather
-const token = process.env['BOT_TOKEN'];
 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
-
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
-
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
-});
 
 
 
